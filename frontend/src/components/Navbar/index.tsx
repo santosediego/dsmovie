6c21bf7aa22127541e5f43d14fde515e57ddbad6
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-01-10 21:46:35 -0300

    Box adjustment

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/santosediego/">
+                    <a href="https://github.com/santosediego" target="_blank" rel="noreferrer">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
                             <p className='dsmovie-contact-link'>/santosediego</p>
