1f38f418ed8974a6846e4342e6eb4046a4978c79
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-01-12 19:41:50 -0300

    Find movies

---
+++ backend/src/main/java/com/santosediego/dsmovie/services/MovieService.java
@@ -0,0 +1,31 @@
+package com.santosediego.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.santosediego.dsmovie.dto.MovieDTO;
+import com.santosediego.dsmovie.entities.Movie;
+import com.santosediego.dsmovie.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		Page<Movie> result = repository.findAll(pageable);
+		Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
+		return page;
+	}
+
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Movie result = repository.findById(id).get();
+		return new MovieDTO(result);
+	}
+}
